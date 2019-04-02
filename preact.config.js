75050fb30bb5342df4ff33358bed712c846a026e
Derek Davenport <derekdavenport@gmail.com>
2019-04-02 11:57:13 -0400

    updated packages and added typescript

---
+++ preact.config.js
@@ -0,0 +1,15 @@
+import preactCliTypeScript from 'preact-cli-plugin-typescript'
+
+export default (config, env, helpers) => {
+	preactCliTypeScript(config);
+	
+  delete config.entry.polyfills;
+  config.output.filename = "[name].js";
+
+  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+  plugin.options.disable = true;
+
+  if (env.production) {
+    config.output.libraryTarget = "umd";
+  }
+};
